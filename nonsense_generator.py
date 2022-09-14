05246c35858d2f6ba494e3c6b4e3db005f2ab890
tomglove <mrethol@gmail.com>
2022-09-14 12:50:50 +0200

    Initial code
    Initial code

---
+++ nonsense_generator.py
@@ -0,0 +1,10 @@
+# The Star Trek writers are on strike and you need some new Sci-Fi sounding words that the actors can talk about
+# https://www.youtube.com/watch?v=h4idB5KAfyc
+import random
+
+words_1 = ["Hyperdimentional", "Electron", "hyperspace", "Pocket Universe", "Photon", "Wave", "Beam"]
+words_2 = ["Combuster", "Emitter", "Dampener", "Array", "Android", "Cyborg", "Torpedo", "Sail", "Terraformner",
+           "Elevator"]
+
+
+print(random.choice(words_1) + " " + random.choice(words_2))
