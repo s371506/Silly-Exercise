d120d474234a485ab40a107170ca12d8651df21a
s371107 <s371107@oslomet.no>
2022-09-15 13:40:19 +0200

    Update laden_swallow_calculator.py

--- laden_swallow_calculator.py
+++ laden_swallow_calculator.py
@@ -3,4 +3,5 @@
 def calc(coconut=454, swallow_capacity=20):
   print(coconut / swallow_capacity)
   
+  print(f"It would require at least {coconut/swallow_capacity} swallows to carry a {coconut}g coconut."
 calc()
